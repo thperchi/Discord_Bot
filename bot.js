6b1633a246f35df2e41eb729c37d7f9442badbec
thperchi <thibault.perchicot@outlook.fr>
2021-04-17 18:32:27 +0200

    +msg auto

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./db/config.json');
-const sardo = require('./sardo');
+const commands = require('./commands');
 const game = require('./game');
 const role = require('./roles');
 
@@ -13,8 +13,13 @@ client.on('message', async msg => {
 	if (!msg.author.bot) {
 		if (msg.content === '!play') game.play(msg, client);
 		if (msg.content.startsWith('.')) role.role(msg, client);
-		sardo.rep(msg);
+		commands.rep(msg);
 	}
 })
+setInterval(function pikaDrink() {
+	let chan = client.channels.cache.get('566668444683206658');
+	pika = chan.guild.members.cache.get('489836698394427392');
+	chan.send(`${pika} bois de l'eau patate.`);
+}, 1000 * 60 * 60);
 
 client.login(config.token);
